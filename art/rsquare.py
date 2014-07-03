a2df0827fcdb0ac0b98150db00a7ae9fc622e0df
Alex King <alex_w_king@yahoo.com>
2014-07-03 17:57:40 -0500

    improvements to rsquare (it was a bit ugly)

--- art/rsquare.py
+++ art/rsquare.py
@@ -2,21 +2,21 @@ from opc.colors import *
 from opc.hue import getHueGen
 from opc.matrix import OPCMatrix
 
-import dpyinfo
 from math import sin, cos, pi
+
+SCALE = 4
 
 class Art:
 
-    description = "Use a higher resolution image downsampled to giver better clarity"
+    description = "Use a higher resolution image downsampled to improve perceived clarity"
 
     def __init__(self, matrix):
         self.hue = getHueGen(0.001)
         self.theta = 0.0
-        self.scale = 4
-        self.matrix = OPCMatrix(self.scale*dpyinfo.WIDTH, self.scale*dpyinfo.HEIGHT, None, True)
-        self.x = self.scale * dpyinfo.WIDTH / 2
-        self.y = self.scale * dpyinfo.HEIGHT / 2
-        self.r = 6.5 * self.scale
+        self.matrix = OPCMatrix(SCALE*matrix.width, SCALE*matrix.height, None, True)
+        self.x = SCALE * matrix.width / 2.0
+        self.y = SCALE * matrix.height / 2.0
+        self.r = 6.5 * SCALE
 
     def irnd(self, n):
         return int(round(n))
@@ -24,7 +24,7 @@ class Art:
     def poly(self, r):
         tc = cos(self.theta)
         ts = sin(self.theta)
-        
+
         poly = [
                 (self.irnd(self.x + r * tc - r * ts) ,  self.irnd(self.y + r * tc  + r * ts)), # UL
                 (self.irnd(self.x - r * tc - r * ts) ,  self.irnd(self.y + r * tc  - r * ts)), # UR
@@ -35,13 +35,14 @@ class Art:
         return poly
 
     def start(self, matrix):
-        matrix.setFirmwareConfig(nointerp=True)
+        pass
 
     def refresh(self, matrix):
-        self.theta += 0.005
+        self.theta += 0.05
         color = self.hue.next()
-        self.matrix.clear()
-        for polys in range(self.scale*7):
+        self.matrix.shift(dv=0.8)
+
+        for polys in range(0, SCALE*7, 2):
             self.matrix.drawPoly(self.poly(self.r-0.2*polys), color)
 
         matrix.copy(self.matrix)
