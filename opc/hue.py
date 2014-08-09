26ddea4f8961e61f241cfc8afbdc419b885ee940
Alex King <alex_w_king@yahoo.com>
2014-08-09 14:33:13 -0500

    added memoize with template caching for hue code

--- opc/hue.py
+++ opc/hue.py
@@ -1,12 +1,17 @@
-from math import fmod
 import colorsys
+from math import fmod
+from utils.mwt import MWT
+from utils.prof import timefunc
 
-
+@MWT(timeout=20)
+@timefunc
 def hsvToRgb(h, s=1.0, v=1.0):
     r, g, b = colorsys.hsv_to_rgb(h, s, v)
     return r*255, g*255, b*255
 
 
+@MWT(timeout=20)
+@timefunc
 def rgbToHsv(r, g, b):
     return colorsys.rgb_to_hsv(r/255, g/255, b/255)
 
@@ -19,6 +24,7 @@ def getHueGen(step=0.05, sat=1, val=1):
         yield hsvToRgb(hue, sat, val)
 
 
+@MWT(timeout=20)
 def hue(shade):
     hue = shade * 255
     if hue < 85:
