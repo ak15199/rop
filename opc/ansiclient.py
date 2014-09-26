97d60837f4f221d10ca32b559b091982a9c8a781
Thom Gerdes <tgerdes@riverbed.com>
2014-09-25 20:14:34 -0700

    Switch color lookup to a list instead of dict
    This will hopefully allow switching to higher number of colors easier.

--- opc/ansiclient.py
+++ opc/ansiclient.py
@@ -48,35 +48,36 @@ class AnsiClient:
         COLOR_MAGENTA = 6
         COLOR_CYAN = 7
 
-        self.colors = {
-                "000":    curses.color_pair(COLOR_BLACK),
-                "001":    curses.color_pair(COLOR_BLUE),
-                "002":    curses.color_pair(COLOR_BLUE)+curses.A_BOLD,
-                "010":    curses.color_pair(COLOR_GREEN),
-                "011":    curses.color_pair(COLOR_CYAN),
-                "012":    curses.color_pair(COLOR_CYAN),                   # approx
-                "020":    curses.color_pair(COLOR_GREEN)+curses.A_BOLD,
-                "021":    curses.color_pair(COLOR_CYAN),                   # approx
-                "022":    curses.color_pair(COLOR_CYAN)+curses.A_BOLD,
-                "100":    curses.color_pair(COLOR_RED),
-                "101":    curses.color_pair(COLOR_MAGENTA),
-                "102":    curses.color_pair(COLOR_MAGENTA),                # approx
-                "110":    curses.color_pair(COLOR_YELLOW),
-                "111":    curses.color_pair(COLOR_WHITE),
-                "112":    curses.color_pair(COLOR_BLUE)+curses.A_BOLD,     # approx
-                "120":    curses.color_pair(COLOR_YELLOW),                 # approx
-                "121":    curses.color_pair(COLOR_GREEN)+curses.A_BOLD,    # approx
-                "122":    curses.color_pair(COLOR_CYAN)+curses.A_BOLD,     # approx
-                "200":    curses.color_pair(COLOR_RED)+curses.A_BOLD,
-                "201":    curses.color_pair(COLOR_RED)+curses.A_BOLD,      # approx
-                "202":    curses.color_pair(COLOR_MAGENTA)+curses.A_BOLD,
-                "210":    curses.color_pair(COLOR_YELLOW),                 # approx
-                "211":    curses.color_pair(COLOR_RED)+curses.A_BOLD,      # approx
-                "212":    curses.color_pair(COLOR_MAGENTA)+curses.A_BOLD,  # approx
-                "220":    curses.color_pair(COLOR_YELLOW)+curses.A_BOLD,
-                "221":    curses.color_pair(COLOR_YELLOW)+curses.A_BOLD,   # approx
-                "222":    curses.color_pair(COLOR_WHITE)+curses.A_BOLD,
-            }
+        self.colors = [
+            curses.color_pair(COLOR_BLACK),
+            curses.color_pair(COLOR_BLUE),
+            curses.color_pair(COLOR_BLUE)+curses.A_BOLD,
+            curses.color_pair(COLOR_GREEN),
+            curses.color_pair(COLOR_CYAN),
+            curses.color_pair(COLOR_CYAN),                   # approx
+            curses.color_pair(COLOR_GREEN)+curses.A_BOLD,
+            curses.color_pair(COLOR_CYAN),                   # approx
+            curses.color_pair(COLOR_CYAN)+curses.A_BOLD,
+            curses.color_pair(COLOR_RED),
+            curses.color_pair(COLOR_MAGENTA),
+            curses.color_pair(COLOR_MAGENTA),                # approx
+            curses.color_pair(COLOR_YELLOW),
+            curses.color_pair(COLOR_WHITE),
+            curses.color_pair(COLOR_BLUE)+curses.A_BOLD,     # approx
+            curses.color_pair(COLOR_YELLOW),                 # approx
+            curses.color_pair(COLOR_GREEN)+curses.A_BOLD,    # approx
+            curses.color_pair(COLOR_CYAN)+curses.A_BOLD,     # approx
+            curses.color_pair(COLOR_RED)+curses.A_BOLD,
+            curses.color_pair(COLOR_RED)+curses.A_BOLD,      # approx
+            curses.color_pair(COLOR_MAGENTA)+curses.A_BOLD,
+            curses.color_pair(COLOR_YELLOW),                 # approx
+            curses.color_pair(COLOR_RED)+curses.A_BOLD,      # approx
+            curses.color_pair(COLOR_MAGENTA)+curses.A_BOLD,  # approx
+            curses.color_pair(COLOR_YELLOW)+curses.A_BOLD,
+            curses.color_pair(COLOR_YELLOW)+curses.A_BOLD,   # approx
+            curses.color_pair(COLOR_WHITE)+curses.A_BOLD,
+        ]
+        self.c_mod = 3
 
         color = [
                 curses.COLOR_WHITE,
@@ -106,13 +107,15 @@ class AnsiClient:
 
     @timefunc
     def _colorindex(self, pixels):
-        return np.char.mod('%c', np.ceil(pixels/128).astype(dtype=np.uint8)+48)
+        return np.ceil(np.dot(pixels, (self.c_mod - 1)) / 256)
 
     @timefunc
     def _color(self, indices):
         shape = indices.shape
         reshaped = indices.reshape(shape[0]*shape[1], shape[2])
-        pixels = [self.colors["".join(pixel)] for pixel in reshaped]
+        pixels = [self.colors[int(sum(self.c_mod**(self.c_mod - i -1) * v
+                              for i,v in enumerate(pixel)))]
+                  for pixel in reshaped]
 
         return np.asarray(pixels).reshape(shape[0], shape[1])
 
