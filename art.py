9ebb78a2b5908035b4f7d1ea3ec25cde22aa99d0
Alex King <alex_w_king@yahoo.com>
2014-11-05 19:27:22 -0800

    improved exception handling, configurable cycle counts

--- art.py
+++ art.py
@@ -1,3 +1,4 @@
+from exceptions import KeyboardInterrupt
 from random import seed
 import sys
 from time import sleep, time
@@ -6,9 +7,10 @@ from traceback import format_exception
 import dpyinfo
 from importer import ImportPlugins
 from opc.matrix import OPCMatrix
-from opc.error import TtyTooSmall
+from opc.utils.prof import dumptimings
 
-FLIPTIME = 30
+DFLT_FLIPTIME_SECS = 30
+DFLT_CYCLE_COUNT = None
 
 matrix = None
 
@@ -23,35 +25,41 @@ def exceptionHandler(etype, evalue, etraceback):
     for line in format_exception(etype, evalue, etraceback):
         logging.error('Exception: '+line.rstrip('\n'))
 
-    if type(etype) is type(TtyTooSmall):
+    if etype is not KeyboardInterrupt:
         print "%s (see log for details)" % evalue
+
 
 def main():
     global matrix
 
     sys.excepthook = exceptionHandler
 
+    cycleCount = 0
     matrix = OPCMatrix(dpyinfo.WIDTH, dpyinfo.HEIGHT,
                         dpyinfo.ADDRESS, dpyinfo.ZIGZAG)
     arts = ImportPlugins("art", ["template.py"], sys.argv[1:], matrix)
 
     if len(arts) == 0:
-        logging.error("Couldn't find any art to execute")
+        matrix.terminate()
+        print "Couldn't find any art to execute"
         exit(1)
 
-    sleep(3)
-
-    while True:
+    while DFLT_CYCLE_COUNT is None or cycleCount < DFLT_CYCLE_COUNT:
+        cycleCount += 1
         seed(time())
 
         for art in arts:
             matrix.setFirmwareConfig()
             art.start(matrix)
             t = time()
-            while time()-t < FLIPTIME:
+            while time()-t < DFLT_FLIPTIME_SECS:
                 art.refresh(matrix)
                 matrix.show()
                 sleep(art.interval()/1000.0)
 
+    matrix.terminate()
+
+
 if __name__ == "__main__":
     main()
+    dumptimings()
