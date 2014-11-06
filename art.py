7abfda0aff495b831088a50e1aa51e3d0f6e4c14
Alex King <alex_w_king@yahoo.com>
2014-11-05 19:54:01 -0800

    added argparse support

--- art.py
+++ art.py
@@ -1,3 +1,4 @@
+import argparse
 from exceptions import KeyboardInterrupt
 from random import seed
 import sys
@@ -32,19 +33,31 @@ def exceptionHandler(etype, evalue, etraceback):
 def main():
     global matrix
 
-    sys.excepthook = exceptionHandler
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--count", type=int,
+            help="run for count cycles through all of the art",
+            default=DFLT_CYCLE_COUNT)
+    parser.add_argument("-f", "--fliptime", type=int,
+            help="run each art for fliptime seconds before transitioning to the next",
+            default=DFLT_FLIPTIME_SECS)
+    parser.add_argument("-p", "--profile",
+            help="switch on and report profiling detail",
+            action="store_true")
+    parser.add_argument("art", help="Optional list of arts",
+            nargs="*")
+    args = parser.parse_args()                                                  
+                                            
     cycleCount = 0
     matrix = OPCMatrix(dpyinfo.WIDTH, dpyinfo.HEIGHT,
                         dpyinfo.ADDRESS, dpyinfo.ZIGZAG)
-    arts = ImportPlugins("art", ["template.py"], sys.argv[1:], matrix)
 
+    arts = ImportPlugins("art", ["template.py"], args.art, matrix)
     if len(arts) == 0:
         matrix.terminate()
         print "Couldn't find any art to execute"
         exit(1)
 
-    while DFLT_CYCLE_COUNT is None or cycleCount < DFLT_CYCLE_COUNT:
+    while args.count is None or cycleCount < args.count:
         cycleCount += 1
         seed(time())
 
@@ -52,14 +65,18 @@ def main():
             matrix.setFirmwareConfig()
             art.start(matrix)
             t = time()
-            while time()-t < DFLT_FLIPTIME_SECS:
+            while time()-t < args.fliptime:
                 art.refresh(matrix)
                 matrix.show()
                 sleep(art.interval()/1000.0)
 
     matrix.terminate()
 
+    if args.profile:
+        dumptimings()
+
 
 if __name__ == "__main__":
+    sys.excepthook = exceptionHandler
+
     main()
-    dumptimings()
