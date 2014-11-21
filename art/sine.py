2f7a388ada1865014bcdbc2c233601467eec0342
Alex King <alex_w_king@yahoo.com>
2014-11-20 22:26:19 -0600

    added sine art

---
+++ art/sine.py
@@ -0,0 +1,38 @@
+import math
+from random import random
+
+from opc.hue import getHueGen
+from opc.scaledmatrix import ScaledMatrix
+
+
+class Art(object):
+
+    description = "Rolling sine wave marks border between contrasting colors"
+
+    def __init__(self, matrix):
+        self.matrix = ScaledMatrix(matrix)
+        self.hue1 = getHueGen(step=0.00001, hue=0.0)
+        self.hue2 = getHueGen(step=0.00001, hue=0.5)
+
+        self.phase = 0
+
+    def start(self, matrix):
+        pass
+
+    def refresh(self, matrix):
+        offset = self.matrix.scale/2
+
+        for x in range(self.matrix.width):
+            angle = 2 * math.pi * x/self.matrix.width
+            y = self.matrix.midHeight + self.matrix.midHeight * math.sin(angle+self.phase)
+
+            self.matrix.drawLine(x, y, x, y-offset, self.hue1.next())
+            self.matrix.drawLine(x, y, x, y+offset, self.hue2.next())
+
+        self.phase += 0.05
+
+        self.matrix.scaleDown()
+  
+    def interval(self):
+        return 100
+
