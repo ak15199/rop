1a37303f519da8c8c7a09359eb7f15986527d907
Alex King <alex_w_king@yahoo.com>
2014-11-27 04:30:41 +0000

    profiling improvements

--- opc/utils/prof.py
+++ opc/utils/prof.py
@@ -1,60 +1,83 @@
 import time
 import logging
 
+active = False
 records = {}
 
 class Record(object):
 
-    def __init__(self, func):
+    def __init__(self, func, reference):
+        self.reference = reference
         self.func = func
         self.times = []
 
     def addTime(self, t):
         self.times.append(t)
 
-    def aggregate(self):
+    def presentable(self):
+        return len(self.times)>0 and not self.reference
+
+    def aggregate(self, totaltime):
+        total = sum(self.times)
         return {
                 "func": self.func,
                 "count": len(self.times),
-                "total": sum(self.times),
+                "percent": 100.0/totaltime*total,
+                "total": total,
                 "min":  min(self.times),
                 "avg":  sum(self.times)/len(self.times),
                 "max":  max(self.times),
             }
 
 
-def dumptiming(result):
-    logging.info("prof: %20s %8d %8f %8f %8f %f" % (
+def _dumptiming(result):
+    logging.info("prof: %20s %8d %6.2f %8.4f %8.4f %8.4f %8.4f" % (
         result["func"],
         result["count"],
+        result["percent"],
         result["total"],
         result["min"],
         result["avg"],
         result["max"],
         ))
 
+
+def _totaltime():
+    global records
+
+    peak = 0
+    for func, record in records.iteritems():
+        try:
+            peak = max(peak, sum(record.times))
+        except:
+            logging.info("except 1: %s %s" % (record.name, str(record.times)))
+
+    return peak
+
+
 def dumptimings():
     global records
 
-    logging.info("prof: %d functions recorded", len(records))
+    totaltime = _totaltime()
+
+    results = [record.aggregate(totaltime) for record in records.values() if record.presentable()]
+
+    logging.info("prof: %d functions recorded", len(results))
     logging.info("prof:")
-    logging.info("prof: %20s %8s %8s %8s %8s %s" % (
+    logging.info("prof: %20s %8s %6s %8s %8s %8s %8s" % (
         "Function",
         "Count",
+        "% Time",
         "Tot Time",
         "Min",
         "Avg",
         "Max"
         ))
 
-    results = []
-    for func, record in records.iteritems():
-        results.append(record.aggregate())
+    for result in sorted(results, key=lambda record: record["percent"], reverse=True):
+        _dumptiming(result)
 
-    for result in sorted(results, key=lambda record: record["total"], reverse=True):
-        dumptiming(result)
-
-active = False
+    logging.info("prof:")
 
 
 def on():
@@ -67,11 +90,13 @@ def off():
     active = False
 
 
-def timefunc(f):
+def timefunc(f, reference=False):
+    global records
+
+    records[f.__name__] = Record(f.__name__, reference)
 
     def f_timer(*args, **kwargs):
         global active
-        global records
 
         if not active:
             return f(*args, **kwargs)
@@ -80,12 +105,12 @@ def timefunc(f):
         result = f(*args, **kwargs)
         end = time.time()
 
-        try:
-            records[f.__name__].addTime(end-start)
-        except:
-            records[f.__name__] = Record(f.__name__)
-            records[f.__name__].addTime(end-start)
+        records[f.__name__].addTime(end-start)
 
         return result
 
     return f_timer
+
+
+def timereference(f):
+    return timefunc(f, True)
