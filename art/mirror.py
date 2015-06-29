e3c28b3dce28e53fb6a2e106752eeaab6fcddbf0
Rafe Kaplan <slobberchops@gmail.com>
2015-06-29 10:39:11 -0700

    Video mirror art.  Reads from a video camera and renders to pixels.

---
+++ art/mirror.py
@@ -0,0 +1,38 @@
+__author__ = 'rafe'
+
+import PIL
+import numpy
+
+try:
+    import cv2
+except ImportError:
+    raise ImportError('Must install cv2 for video to work.  PiCamera coming soon.')
+
+capture = cv2.VideoCapture(0)
+
+def get_frame():
+    _, bgr = capture.read()
+    rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+    return PIL.Image.fromarray(rgb)
+
+
+class Art(object):
+
+    description = "Video Mirror"
+
+    def __init__(self, matrix, config):
+        pass
+
+    def start(self, matrix):
+        pass
+
+    def refresh(self, matrix):
+        frame = get_frame().rotate(90).resize((matrix.width, matrix.height), PIL.Image.BILINEAR)
+        image = numpy.asarray(frame)
+        for y in range(matrix.width):
+            for x in range(matrix.height):
+                matrix.drawPixel(x, y, image[x, y])
+
+    def interval(self):
+        return 30
+
