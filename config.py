56ba0d1fa3b8b357eeaa896f685d7fc755516a2b
Rafe Kaplan <slobberchops@gmail.com>
2015-07-19 20:15:01 -0700

    Temporarily removed complex configuration. Only supports OPC for now. Use make_wall.py to create gl-server configuration. Use openpixelcontrol gl-server to simulate mirror wall.

--- config.py
+++ config.py
@@ -1,52 +1,25 @@
 """
 Display Configuration
 """
+
+ENV = 'dev'  # 'dev' == development environment,  'prod' == production environment
+
 # Display Geometry
-# Dimensions of mirror is 6x4
-WIDTH, HEIGHT = 50, 32
+# Dimensions of mirror is 4x6
+WIDTH, HEIGHT = 32, 50
 
 # Device type and address, where 'address' is an optional colon-separated
 # part of the value that is driver dependent
-try:
-    import sdl2
-except ImportError:
-    DRIVER = 'opc:localhost:7890' # OPC driver
-else:
-    DRIVER = 'sdl2window'         # For display in SDL window
+DRIVER = 'opc:localhost:7890'
 
 # Device configuration
 # ZIGZAG = True                 # whether display reverses direction, line to line
 # FLIPUD = True                 # up-down display orientation
 # FLIPLR = True                 # left-right display orientation
 
-selection = 0
-def selector(new=None):
-    global selection
-    if new != None:
-        selection = new
-    return selection
-
 
 """
 Art Configuration Settings
 """
 config = {
-    "COMPOUND": [
-        ('persistent', {
-            "COMPOUND": [
-                ('fade', {}),
-                ('selector', {
-                    "COMPOUND": [
-                        ('life', {}),
-                        ('mirror', {}),
-                    ],
-                    "SELECTOR": selector,
-                }),
-            ]
-        }),
-        ('rotator', {
-            "SELECTOR": selector,
-            "TIMEOUT": 5,
-        })
-    ]
 }
