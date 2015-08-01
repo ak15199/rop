6c7b48c5ccedf3e8bdeb609ada018d26a3f54837
Rafe Kaplan <slobberchops@gmail.com>
2015-08-01 23:40:32 +0000

    filter video for alpha channel

--- art/mirror.py
+++ art/mirror.py
@@ -9,9 +9,11 @@ except ImportError:
     import io
     import picamera
     camera = picamera.PiCamera()
+    camera.resolution = 50, 32
     def get_frame():
         stream = io.BytesIO()
-        camera.capture(stream, format='png')
+        camera.capture(stream, format='png', use_video_port=True)
+        stream.seek(0)
         return PIL.Image.open(stream)
 else:
     capture = cv2.VideoCapture(0)
@@ -19,6 +21,17 @@ else:
         _, bgr = capture.read()
         rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
         return PIL.Image.fromarray(rgb)
+
+
+import contextlib
+import time
+@contextlib.contextmanager
+def timeit():
+    start = time.time()
+    try:
+        yield start
+    finally:
+        print '%03f' % (time.time() - start)
 
 
 class Art(object):
@@ -33,11 +46,12 @@ class Art(object):
 
     def refresh(self, matrix):
         frame = get_frame().rotate(90).resize((matrix.width, matrix.height), PIL.Image.BILINEAR)
-
         image = numpy.asarray(frame)
+        draw_pixel = matrix.drawPixel
         for y in range(matrix.width):
             for x in range(matrix.height):
-                matrix.drawPixel(y, x, image[x, y], 0.3)
+                rgb = image[x, y][:3]  # May need to strip alpha channel
+                draw_pixel(x, y, rgb, 0.3)
 
     def interval(self):
         return 30
