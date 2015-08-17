377783afdbaadd15c6d50e8258ab0c760a5f7fa3
Rafe Kaplan <slobberchops@gmail.com>
2015-08-17 02:10:45 +0000

    removed test scripts; preparing to break out shutdown button from main controllers script

--- controls/controllers.py
+++ controls/controllers.py
@@ -16,16 +16,19 @@
 
 
 import time
+import json
 import collections
 import datetime
 import atexit
+import os
 import random
 import signal
 import sys
 
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 sys.path.append("/home/pi/Adafruit-Raspberry-Pi-Python-Code/Adafruit_ADS1x15")
-import Adafruit_ADS1x15
+from Adafruit_ADS1x15 import ADS1x15
+ADS1015 = 0x00  # 12-bit ADC
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
@@ -35,166 +38,182 @@ BUTTON_PIN = 22
 
 
 def handle_INT(signal, frame):
-	print("SIGINT caught, exiting gracefully.")
-	shutdown(quit=True)
+    print("SIGINT caught, exiting gracefully.")
+    shutdown(quit=True)
 
 def handle_TERM(signal, frame):
-	print("SIGTERM caught, exiting gracefully.")
-	shutdown(halt=True, quit=True)
+    print("SIGTERM caught, exiting gracefully.")
+    halt()
 
-def halt():
-	shutdown(halt=True)
+def halt(ignore=None):
+    shutdown(quit=True, halt=True)
+
+def turnon_arduino():
+    GPIO.output(ARDUINO_PIN, GPIO.HIGH)
+
+def shutdown_arduino():
+    GPIO.output(ARDUINO_PIN, GPIO.LOW)
+
+def turnon_lamp():
+    GPIO.output(LAMP_PIN, GPIO.HIGH)
+
+def shutdown_lamp():
+    GPIO.output(LAMP_PIN, GPIO.LOW)
+
+def shutdown_motors():
+    for hat in hats:
+        hat.shutdown()
+
 
 def shutdown(quit=False, halt=False):
-	for hat in hats:
-		hat.shutdown()
-	GPIO.output(ARDUINO_PIN, GPIO.LOW)
-    GPIO.output(LAMP_PIN, GPIO.LOW)
+    shutdown_motors()
+    shutdown_arduino()
+    shutdown_lamp()
     GPIO.cleanup()
-	if halt:
-	    os.system("sudo shutdown -h now")
-	if quit or halt:
-		sys.exit(0)
+    if halt:
+        os.system("sudo shutdown -h now")
+    if quit or halt:
+        sys.exit(0)
 
 
 class Hat(object):
-	def __init__(self, addr, motorlist, verbose=False):
-		self.name = addr
-		self.verbose = verbose
-		self.hat = Adafruit_MotorHAT(int(addr, 16))
-		self.motors = dict([(m, self.hat.getMotor(m)) for m in motorlist])
-		self.until = dict([(m, None) for m in motorlist])
-		for motorname, motor in self.motors.items():
-			motor.setSpeed(150)
-			motor.run(Adafruit_MotorHAT.FORWARD)
-			motor.run(Adafruit_MotorHAT.RELEASE)
-			if self.verbose:
-				print("init hat %s motor %s" % (self.name, motorname))
+    def __init__(self, addr, motorlist, verbose=False):
+        self.name = addr
+        self.verbose = verbose
+        self.hat = Adafruit_MotorHAT(int(addr, 16))
+        self.motors = dict([(m, self.hat.getMotor(m)) for m in motorlist])
+        self.until = dict([(m, None) for m in motorlist])
+        for motorname, motor in self.motors.items():
+            motor.setSpeed(150)
+            motor.run(Adafruit_MotorHAT.FORWARD)
+            motor.run(Adafruit_MotorHAT.RELEASE)
+            if self.verbose:
+                print("init hat %s motor %s" % (self.name, motorname))
 
-	def shutdown_one(self, motorname):
-		self.until[motorname] = None
-		motor = self.motors[motorname]
-		motor.run(Adafruit_MotorHAT.RELEASE)
-		if self.verbose:
-			print("shutdown hat %s motor %s" % (self.name, motorname))
+    def shutdown_one(self, motorname):
+        self.until[motorname] = None
+        motor = self.motors[motorname]
+        motor.run(Adafruit_MotorHAT.RELEASE)
+        if self.verbose:
+            print("shutdown hat %s motor %s" % (self.name, motorname))
 
-	def shutdown(self):
-		for motorname, motor in self.motors.items():
-			self.shutdown_one(motorname)
+    def shutdown(self):
+        for motorname, motor in self.motors.items():
+            self.shutdown_one(motorname)
 
-	def run(self, motorname, direction, speedpct, text=""):
-		if speedpct < 1:
-			self.motors[motorname].run(Adafruit_MotorHAT.RELEASE)
-			if self.verbose:
-				print("hat %s motor %s resting %s" % (self.name, motorname, text))
-		else:
-			direction = Adafruit_MotorHAT.FORWARD if direction[0].upper() == "F" else Adafruit_MotorHAT.BACKWARD
-			speed = int((speedpct if speedpct < 1.0 else speedpct / 100.0) * 253)
-			self.motors[motorname].run(direction)
-			self.motors[motorname].setSpeed(speed)
-			if self.verbose:
-				print("hat %s motor %s running %s at speed %s %s" %
-						(self.name, motorname, direction, speed, text))
+    def run(self, motorname, direction, speedpct, text=""):
+        if speedpct < 1:
+            self.motors[motorname].run(Adafruit_MotorHAT.RELEASE)
+            if self.verbose:
+                print("hat %s motor %s resting %s" % (self.name, motorname, text))
+        else:
+            direction = Adafruit_MotorHAT.FORWARD if direction[0].upper() == "F" else Adafruit_MotorHAT.BACKWARD
+            speed = int((speedpct if speedpct < 1.0 else speedpct / 100.0) * 253)
+            self.motors[motorname].run(direction)
+            self.motors[motorname].setSpeed(speed)
+            if self.verbose:
+                print("hat %s motor %s running %s at speed %s %s" %
+                        (self.name, motorname, direction, speed, text))
 
-	def run_for(self, motorname, direction, speedpct, runtime=10):
-		self.until[motorname] = time.time() + runtime
-		self.run(motorname, direction, speedpct, text="for %ss" % int(runtime))
+    def run_for(self, motorname, direction, speedpct, runtime=10):
+        self.until[motorname] = time.time() + runtime
+        self.run(motorname, direction, speedpct, text="for %ss" % int(runtime))
 
-	def run_random(self, motorname):
-		direction = "F" if random.getrandbits(1) == 0 else "B"
-		speedpct = abs(int(random.gauss(60, 40)))
-		runtime = abs(random.gauss(6, 3))
-		speedpct = 0 if speedpct < 35 or runtime > 20 else speedpct
-		self.run_for(motorname, direction, speedpct, runtime)
+    def run_random(self, motorname):
+        direction = "F" if random.getrandbits(1) == 0 else "B"
+        speedpct = abs(int(random.gauss(60, 40)))
+        runtime = abs(random.gauss(6, 3))
+        speedpct = 0 if speedpct < 35 or runtime > 20 else speedpct
+        self.run_for(motorname, direction, speedpct, runtime)
 
-	def check_all_and_restart(self):
-		for motorname, until in self.until.items():
-			if until is None or time.time() > until:
-				self.run_random(motorname)
+    def check_all_and_restart(self):
+        for motorname, until in self.until.items():
+            if until is None or time.time() > until:
+                self.run_random(motorname)
 
-	def test_on(self, secs=10):
-		for motorname, motor in self.motors.items():
-			self.run(motorname, 'F', 50,
-				'testing hat %s motor %s at %s' % (self.name, motorname, 50))
-		time.sleep(secs)
-		self.shutdown()
+    def test_on(self, secs=10):
+        for motorname, motor in self.motors.items():
+            self.run(motorname, 'F', 50,
+                'testing hat %s motor %s at %s' % (self.name, motorname, 50))
+        time.sleep(secs)
+        self.shutdown()
 
 
 
 if __name__ == "__main__":
-	signal.signal(signal.SIGINT, handle_INT)
-	signal.signal(signal.SIGTERM, handle_TERM)
+    signal.signal(signal.SIGINT, handle_INT)
+    signal.signal(signal.SIGTERM, handle_TERM)
 
-	GPIO.setup(LAMP_PIN, GPIO.OUT, initial=GPIO.LOW)
-	GPIO.setup(ARDUINO_PIN, GPIO.OUT, initial=GPIO.LOW)
-	GPIO.setup(BUTTON_PIN, GPIO.IN, initial=GPIO.LOW)
-	GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING,
-    	callback=halt, bouncetime=500)
+    GPIO.setup(LAMP_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(ARDUINO_PIN, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(BUTTON_PIN, GPIO.IN, initial=GPIO.LOW)
+    GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING,
+        callback=halt, bouncetime=500)
 
-	verbose = True
+    verbose = True
 
-	ADS1015 = 0x00  # 12-bit ADC
-	adc = ADS1x15(ic=ADS1015)
-	V_per_mV_read = 200.8
+    ADS1015 = 0x00  # 12-bit ADC
+    adc = ADS1x15(ic=ADS1015)
+    V_per_mV_read = 200.8
 
-	hats = [Hat('0x67', [1,2,3,4], verbose=verbose),
-			Hat('0x61', [1,2,3,4], verbose=verbose),
-			Hat('0x60', [1,2,3,4], verbose=verbose),
-			Hat('0x66', [1,2,3,4], verbose=verbose)]
+    hats = [Hat('0x67', [1,2,3,4], verbose=verbose),
+            Hat('0x61', [1,2,3,4], verbose=verbose),
+            Hat('0x60', [1,2,3,4], verbose=verbose),
+            Hat('0x66', [1,2,3,4], verbose=verbose)]
 
-	poll_interval = 1
-	running = False
-	sensed = False
-	log_rec = dict()
-	voltages = collections.deque(maxlen=100)
-	logfile = "/home/pi/var/log/%s.log" % datetime.datetime.now().isoformat().replace(':', '-').split('.')[0]
-	with open(logfile, 'w') as logFH:
-		logFH.write('# beginning log')
+    poll_interval = 1
+    running = False
+    sensed = False
+    log_rec = dict()
+    voltages = collections.deque(maxlen=100)
+    logfile = "/home/pi/var/log/%s.log" % datetime.datetime.now().isoformat().replace(':', '-').split('.')[0]
+    with open(logfile, 'w') as logFH:
+        logFH.write('# beginning log')
 
-	while True:
-	    sensors = {'volts': adc.readADCSingleEnded(0, 4096, 250),
-	               'dist':  adc.readADCSingleEnded(3, 4096, 250)
-	              }
-	    # print "v0=%s, v1=%s, v2=%s" % tuple(volts_single)
-	    measured_V = round(sensors['volts']/V_per_mV_read, 2)
-	    measured_D = round(sensors['dist']/(5.3/512.0) , 0)
-	    voltages.append(measured_V)
-	    mean_V = sum(voltages)/len(voltages)
-	    # print "time: %s. measured V=%s, measured D=%s" % (
-	    #    int(time.time()), measured_V, measured_D)
+    while True:
+        volts = adc.readADCSingleEnded(0, 4096, 250)
+        measured_V = round(volts/V_per_mV_read, 2)
+        # sensors = {'volts': adc.readADCSingleEnded(0, 4096, 250),
+        #            'dist':  adc.readADCSingleEnded(3, 4096, 250)
+        #           }
+        # print "v0=%s, v1=%s, v2=%s" % tuple(volts_single)
+        # measured_V = round(sensors['volts']/V_per_mV_read, 2)
+        # measured_D = round(sensors['dist']/(5.3/512.0) , 0)
+        voltages.append(measured_V)
+        mean_V = sum(voltages)/len(voltages)
+        # print "time: %s. measured V=%s, measured D=%s" % (
+        #    int(time.time()), measured_V, measured_D)
 
-		log_rec = {'timestamp': int(time.time()),
-					'running': running, sensed: sensed,
-					'volts': mean_V, 'distance': meas_D}
-		with open(logfile, 'a') as logFH:
-			logFH.write(json.dumps(log_rec))
+        log_rec = {'timestamp': int(time.time()),
+                    'running': running, sensed: sensed,
+                    'last_volts': volts, 'mean_volts': mean_V}
+        with open(logfile, 'a') as logFH:
+            logFH.write(json.dumps(log_rec))
 
-		# now take action.
-		if mean_V < 11.1:
-			# shut it down!
-			time.sleep(poll_interval * 10)
-			continue
+        # now take action.
+        if mean_V < 11.1:
+            halt()
 
-	 	# change this to poll sensor then write to sensed.
-		with open("/home/pi/sensed", 'r') as presence:
-			sensed = presence.read().strip()[-1]
+        # change this to poll sensor then write to sensed.
+        with open("/home/pi/sensed", 'r') as presence:
+            sensed = presence.read().strip()[-1]
 
-	 	if sensed not in ["0", "1"] or sensed == "0":
-			if running:
-				shutdown(quit=False)
-			running = False
-			time.sleep(poll_interval/2.0)
-			continue
+        if sensed not in ["0", "1"] or sensed == "0":
+            if running:
+                shutdown(quit=False)
+            running = False
+            time.sleep(poll_interval/2.0)
+            continue
 
-		if not running:
-	 		# startup code: bring up IR light, then start motors
-			running = True
+        if not running:
+            # startup code: bring up IR light, then start motors
+            running = True
+            turnon_lamp()
 
+        for hat in hats:
+            hat.check_all_and_restart()
 
-		for hat in hats:
-			hat.check_all_and_restart()
+        # here log what's going on: lamp, sensed, voltage, motor status
+        time.sleep(poll_interval) # possible to go slower?
 
-		# here log what's going on: lamp, sensed, voltage, motor status
-		time.sleep(poll_interval) # possible to go slower?
-
-	shutdown(quit=True)
+    shutdown(quit=True)
--- controls/serialtest.py
+++
@@ -1,6 +0,0 @@
-import serial
-ser = serial.Serial('/dev/ttyAMA0', 9600)
-
-while 1 :
-    line = ser.read(1024*1024)
-    print(line)
