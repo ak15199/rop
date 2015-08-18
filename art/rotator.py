a7379ffdde1db5399e38a23123eb23a2bfa1724b
Rafe Kaplan <slobberchops@gmail.com>
2015-08-17 17:44:21 -0700

    Fixed rotator

--- art/rotator.py
+++ art/rotator.py
@@ -2,34 +2,42 @@ __author__ = 'rafe'
 
 import time
 
+from art import compound
 
-class Art(object):
+
+class Art(compound.Art):
 
     description = "Arts Rotator"
 
     def __init__(self, matrix, config):
+        super(Art, self).__init__(matrix, config)
         self.__timeout = config.get('TIMEOUT', 30)  # defaults to 30 seconds.
-        self.__selector = config.get('SELECTOR', lambda: None)
+        self.__selected = 0
 
     @property
     def selected(self):
-        return self[self.__selection][1]
-
-    def select(self, index):
-        self[index]  # Check index
-        self.__selection = index
+        if len(self):
+            return self[self.__selected][1]
+        else:
+            return None
 
     def start(self, matrix):
+        matrix.clear()
         self.__last_flip = time.time()
+        super(Art, self).start(matrix)
 
     def refresh(self, matrix):
-        elapsed = time.time() - self.__last_flip
-        print elapsed, self.__timeout
+        now = time.time()
+        elapsed = now - self.__last_flip
         if elapsed >= self.__timeout:
-            self.__selector()
-            self.__last_flip = time.time()
-            print self.__selector()
-            self.__selector(self.__selector() + 1)
+            self.__selected = (self.__selected + 1) % len(self)
+            matrix.clear()
+            self.selected.start(matrix)
+            self.__last_flip = now
+        if self.selected:
+            self.selected.refresh(matrix)
+        else:
+            matrix.clear()
 
     def interval(self):
         return 30
