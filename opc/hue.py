6dd3f555e57435e40c9e5ce40dde51787d8e7dce
Alex <alex_w_king@yahoo.com>
2018-02-11 14:55:20 -0600

    Improved hsvToRgb color curves

--- opc/hue.py
+++ opc/hue.py
@@ -1,19 +1,39 @@
 import colorsys
-from math import fmod
+from math import fmod, sin, pi
 from mwt import mwt
 from .utils.prof import timefunc
 
 
 @mwt(timeout=20)
 @timefunc
-def hsvToRgb(h, s=1.0, v=1.0):
-    r, g, b = colorsys.hsv_to_rgb(h, s, v)
-    return r*255, g*255, b*255
+def hsvToRgb(h, s=1.0, v=1.0, rainbow=True):
+    """
+    Convert a (h, s, v) value to the (r, g, b) color space. By default,
+    we use a more cpu-intense method to curve the hue ramps above the
+    typical linear, with a view to evening out the visible range of yellow,
+    cyan, and purple.
+
+    This method takes h, s, and v values in range 0..1 and returns r, g,
+    and b values in range 0..255.
+    """
+    if rainbow:
+        return [sin(c*pi/2)*255 for c in colorsys.hsv_to_rgb(h, s, v)]
+
+    return [c*255 for c in colorsys.hsv_to_rgb(h, s, v)]
 
 
 @mwt(timeout=20)
 @timefunc
 def rgbToHsv(r, g, b):
+    """
+    Convert a (r, g, b) value to the (h, s, v) color space. Note that
+    presently, converting a rgb value to hsv and then back again will
+    yield different results from the expected, because of the way in
+    which color curves work in hsvToRgb
+
+    This method takes r, g, and b values in range 0..255 and returns
+    h, s, v values in range 0..1.
+    """
     return colorsys.rgb_to_hsv(r/255, g/255, b/255)
 
 
