3f48d18831b6d24fb615a1a830888c1ee2d653b4
Alex <alex_w_king@yahoo.com>
2018-03-24 21:24:37 -0500

    Tuning/cleanup to RGB Plasma

--- art/rgbplasma.py
+++ art/rgbplasma.py
@@ -7,33 +7,24 @@ import numpy as np
 
 class Channel(object):
 
-    def __init__(self, matrix, locked):
+    def __init__(self, matrix):
         ones = np.ones(matrix.numpix).reshape((matrix.height, matrix.width))
         self.x = ones*np.arange(matrix.width)
         self.y = np.flipud(np.rot90(np.rot90(ones)*np.arange(matrix.height)))
 
-        self.base = random()*128000
-        if locked:
-            self.delta = 0.7
-        else:
-            self.delta = random()*0.5+0.2 
+        self.delta = random()*0.5+0.2 
 
-    def _dist(self, a, b, c, d):
-        return np.sqrt((c-a)*(c-a)+(d-b)*(d-b))
+    def refresh(self, matrix):
+        self.x += self.delta
+        self.y += self.delta
 
-    def refresh(self):
-        self.base += self.delta
-
-        xb = self.x + self.base
-        yb = self.y + self.base
-
-        c1 = self._dist(xb, self.y, 128.0, 128.0) / 8.0
-        c2 = self._dist(self.y, self.y, 64.0, 64.0) / 8.0
-        c3 = self._dist(self.x, yb / 7, 192.0, 64) / 7.0
-        c4 = self._dist(self.y, self.x, 192.0, 100.0) / 8.0
+        c1 = self.x/16
+        c2 = self.y/8
+        c3 = (self.x+self.y)/16
+        c4 = np.sqrt((self.x*self.x)+(self.y*self.y))/8
 
         channel = np.sum(np.sin(c) for c in (c1, c2, c3, c4))
-        return 255*np.fmod(np.fabs(1+channel/2), 1.0)
+        return np.fmod(np.fabs(1+channel/2), 1.0)
 
 
 class Art(ArtBaseClass):
@@ -41,16 +32,15 @@ class Art(ArtBaseClass):
     description = "Plasma by RGB channel"
 
     def __init__(self, matrix, config):
-        locked = random()<0.5
         with HQ(matrix):
-            self.channels = [Channel(matrix, locked) for channel in range(3)]
+            self.channels = [Channel(matrix) for channel in range(3)]
 
     def start(self, matrix):
         matrix.hq()
 
     def refresh(self, matrix):
 
-        channels = [channel.refresh() for channel in self.channels]
+        channels = [255*channel.refresh(matrix) for channel in self.channels]
 
         for y in range(matrix.height):
             for x in range(matrix.width):
@@ -61,4 +51,4 @@ class Art(ArtBaseClass):
                     ))
 
     def interval(self):
-        return 60
+        return 270
