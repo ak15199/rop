e6e6d3f2c7ee0e6f43970363beafdfcaaa23de52
Alex <alex_w_king@yahoo.com>
2019-08-08 23:44:54 +0000

    Fixed some bugs and utilized more numpy in Voronoi

--- art/voronoi.py
+++ art/voronoi.py
@@ -6,19 +6,19 @@ import numpy as np
 import random
 import math
 
+
 class Art(ArtBaseClass):
 
     description = "Voronoi Animation"
 
     def __init__(self, matrix, config):
         self.field = None
-        self.count = int(math.sqrt(matrix.numpix)/3)
+        self.count = int(math.sqrt(math.sqrt(matrix.numpix)))
         self.regions = self._seed(matrix)
         self.huebase = 0
 
     def _delta(self, size):
-        r = math.sqrt(size)/5
-
+        r = 0.1
         d = 0
         while not d:
             d = random.uniform(-r, r)
@@ -27,59 +27,68 @@ class Art(ArtBaseClass):
 
     def _seed(self, matrix):
         return [{
-                "nx": random.randrange(matrix.width),
-                "ny": random.randrange(matrix.height),
+                "x": random.randrange(matrix.width),
+                "y": random.randrange(matrix.height),
 
-                "ndx": self._delta(matrix.width),
-                "ndy": self._delta(matrix.height),
+                "dx": self._delta(matrix.width),
+                "dy": self._delta(matrix.height),
                 } for i in range(self.count)]
 
     def start(self, matrix):
         matrix.setFirmwareConfig(nointerp=True)
-        matrix.hq()
         matrix.clear()
 
-    def _bounce(self, v, s, vmin, vmax):
-        if v<=vmin:
-            return abs(s)
-        if v>=vmax:
-            return -abs(s)
-        return s
+    def _bounce(self, v, step, vmax):
+        if v<=1:
+            return abs(step)
+        if v>=vmax-step:
+            return -abs(step)
+
+        return step
+
+    def _closest(self, xs, ys, x, y):
+        hypots = np.hypot(xs-x, ys-y)
+
+        return np.where(hypots == np.amin(hypots))[0][0]
 
     def refresh(self, matrix):
         self.huebase = self.huebase + 0.001
 
+        # canvas for us to write to
         field = np.empty((matrix.width, matrix.height))
 
+        # linearize x and y coordinates for regions
+        xs = np.array([region["x"] for region in self.regions])
+        ys = np.array([region["y"] for region in self.regions])
+
+        # find closest region to eack pixel
         for y in range(matrix.height):
             for x in range(matrix.width):
-                dmin = math.hypot(matrix.width-1, matrix.height-1)
-                j = -1
-                for i in range(self.count):
-                    d = math.hypot(self.regions[i]["nx"]-x, self.regions[i]["ny"]-y)
-                    if d < dmin:
-                        dmin = d
-                        j = i
+                c=self._closest(xs, ys, x, y)
+                field[x,y] = self.huebase+c/self.count
 
-                field[x,y] = self.huebase+j/self.count
-
+        # update region coordinates
         self.regions = [{
-                "nx": region["nx"]+region["ndx"],
-                "ny": region["ny"]+region["ndy"],
+                "dx": self._bounce(region["x"], region["dx"],  matrix.width-1),
+                "dy": self._bounce(region["y"], region["dy"],  matrix.height-1),
 
-                "ndx": self._bounce(region["nx"], region["ndx"], 0, matrix.width),
-                "ndy": self._bounce(region["ny"], region["ndy"], 0, matrix.height),
-                } for region in self.regions]
+                "x": region["x"]+region["dx"],
+                "y": region["y"]+region["dy"],
+            } for region in self.regions]
 
+        # blend old and new
         if self.field is not None:
             self.field = (self.field*0.3) + (field*0.7)
         else:
             self.field = field
 
+        # draaw
         for y in range(matrix.height):
             for x in range(matrix.width):
                 matrix.drawPixel(x, y, hsvToRgb(self.field[x,y], 1, 1))
 
-     
+        for region in self.regions:
+            matrix.drawPixel(region["x"], region["y"], hsvToRgb(1,  0, 0))
+
     def interval(self):
-        return 100
+        return 50
